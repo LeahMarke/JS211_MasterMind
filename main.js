e416d2a83b26ab4a9b89258362791d1ce81f8c00
leahmarke <lmarke@live.com>
2022-07-21 01:29:11 -0400

    working

--- main.js
+++ main.js
@@ -30,31 +30,67 @@ const getRandomInt = (min, max) => {
 
 const generateHint = (guess) => {
   let solutionArray = solution.split("");
+  // console.log("The solution array is:", solutionArray);
   let guessArray = guess.split("");
+  // console.log("The guess array is:", guessArray);
+
   let correctLetterLocations = 0;
   let correctLetters = 0;
-  for (let i = 0; i < 4; i++) {
-    if (guessArray[i] == solutionArray[i]) {
+  solutionArray.forEach((letter, index) => {
+    // console.log(
+    //   `Does the solution letter: ${letter} equal the guess letter: ${guessArray[index]}`
+    // );
+    if (letter === guessArray[index]) {
       correctLetterLocations++;
-      solutionArray[i] = "Z";
-    } else if (solutionArray.indexOf(guessArray[i]) > -1) {
-      correctLetters++;
-      solutionArray[solutionArray.indexOf(guessArray[i])] = "X";
+      solutionArray[index] = null;
     }
-  }
-  let targetIndex = solutionArray.indexOf(guessArray[i]);
-  return `${correctLetterLocations}-${correctLetters}`;
+    let targetIndex = solutionArray.indexOf(guessArray[index]);
+    // console.log(
+    //   "Target index: index of solution that matches guess:",
+    //   targetIndex
+    // );
+    if (targetIndex > -1) {
+      correctLetters++;
+      solutionArray[solutionArray.indexOf(guessArray[index])] = null;
+    }
+    // console.log(guessArray[index]);
+    // console.log(index);
+    // console.log(solutionArray);
+  });
+  // console.log("number of letters in correct location:", correctLetterLocations);
+  let hint = `${correctLetterLocations}-${correctLetters}`;
+  board.push(`${guess}, ${hint}`);
+  return hint;
+  // for (let i = 0; i < 4; i++) {
+  //   if (guessArray[i] == solutionArray[i]) {
+  //     correctLetterLocations++;
+  //     solutionArray[i] = "Z";
+  //   } else if (solutionArray.indexOf(guessArray[i]) > -1) {
+  //     correctLetters++;
+  //     solutionArray[solutionArray.indexOf(guessArray[i])] = "X";
+  //   }
+  // }
+  // let targetIndex = solutionArray.indexOf(guessArray[i]);
+  // return `${correctLetterLocations}-${correctLetters}`;
 };
 
 const mastermind = (guess) => {
   solution = "abcd"; // Comment this out to generate a random solution
-  let hint = generateHint(guess);
-  board = board.push(`${guess}, ${hint}`);
-  if (board.length == 10) {
-    return `You ran out of turns! The solution was ${solution}`;
+  if (guess === solution) {
+    return `You guessed it!`;
   } else {
-    return `Guess again...`;
+    generateHint(guess);
   }
+  // let hint = generateHint(guess);
+  // board = board.push(`${guess}, ${hint}`);
+  // if (board.length == 10) {
+  //   return `You ran out of turns! The solution was ${solution}`;
+  // } else {
+  //   return `Guess again...`;
+  // }
+  // ?start game
+  // ?determine winner
+  // ?determine loser
 };
 
 const getPrompt = () => {
